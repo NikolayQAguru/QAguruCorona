b5be7be73dfbd4d39c1b0870a7acc5dcc48abc5f
NikolajsGromovs <nik.gromov@inbox.lv>
2020-11-12 20:00:49 +0200

    line 13 space added

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -10,7 +10,7 @@ public class MyFirstTest {
 
     @Test
     public void firstTest() {
-        System.out.println("Hello, corona!");
+        System.out.println("Hello, corona! ");
         int sum = sumTwoDigits(15, 20);
         System.out.println("And the sum is: " + sum);
         System.out.println(sumTwoDigits(3, 8));
